8f795cd7598e8ef4900b6b69eae286ac982dae5c
Saqib Hassan <134485047+Saqib-mir@users.noreply.github.com>
2023-12-16 23:20:39 +0530

    Add files via upload

---
+++ src/components/Data/QuizData.js
@@ -0,0 +1,158 @@
+export const QuizData = [
+    {
+        question: "Which language runs in a web browser?",
+        options:[ "Java", "C", "Python", "JavaScript"],
+        answer: 4
+    },
+    {
+        question: "What does CSS stand for?",
+        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
+    },
+    {
+        question: "Who is the father of cloud computing",
+        options: ["Sharon B. Codd",
+             "Edgar Frank Codd",
+             "J.C.R. Licklider",
+             "Charles Bachman"],
+             answer:3
+    
+    },
+
+    {
+        question: " Which of the following is not a type of cloud server?",
+        options: ["Public Cloud Servers",
+             "Private Cloud Servers",
+             " Dedicated Cloud Servers",
+             "Merged Cloud Servers"],
+             answer:4
+    
+    },
+
+
+
+    {
+        question: "What does IaaS stand for?",
+        options: ["Infrastructure as a Service", "Internet as a Service", "Integration as a Service", "Innovation as a Service"],
+        answer: 1,
+      },
+      {
+        question: "Which company offers the AWS (Amazon Web Services) cloud platform?",
+        options: ["Microsoft", "Google", "Amazon", "IBM"],
+        answer: 3,
+      },
+    
+      // JavaScript Questions
+      {
+        question: "What is the purpose of the 'typeof' operator in JavaScript?",
+        options: ["Checking if a variable is defined", "Identifying the type of a variable", "Concatenating strings", "Creating new variables"],
+        answer: 2,
+      },
+      {
+        question: "What is a closure in JavaScript?",
+        options: ["A function with no parameters", "A way to close a browser window", "A function with access to variables from its containing scope", "A special type of loop"],
+        answer: 3,
+      },
+    
+      // Operating Systems Questions
+      {
+        question: "What is the role of the operating system?",
+        options: ["Running applications", "Managing hardware resources", "Providing a graphical user interface", "Sending emails"],
+        answer: 2,
+      },
+      {
+        question: "Which operating system is open source?",
+        options: ["Windows", "macOS", "Linux", "Unix"],
+        answer: 3,
+      },
+    
+      // Additional Questions
+      {
+        question: "In object-oriented programming, what is inheritance?",
+        options: ["A way to create instances of an object", "The process of creating classes", "The ability of a class to inherit properties and methods from another class", "A design pattern for databases"],
+        answer: 3,
+      },
+      {
+        question: "What is the purpose of the 'git' version control system?",
+        options: ["Managing project dependencies", "Collaborative coding", "Tracking changes in source code", "Generating random numbers"],
+        answer: 3,
+      },
+
+     
+
+      {
+        question: "What is the main benefit of serverless computing?",
+        options: ["Lower cost", "No need for servers", "Better performance", "Easier debugging"],
+        answer: 2,
+      },
+      {
+        question: "What is a virtual machine in cloud computing?",
+        options: ["A computer program", "A physical server", "A simulated computer within a computer", "A cloud storage service"],
+        answer: 3,
+      },
+      {
+        question: "What is the purpose of a CDN (Content Delivery Network) in cloud computing?",
+        options: ["Secure data storage", "Optimizing website performance", "Managing user authentication", "Creating virtual networks"],
+        answer: 2,
+      },
+    
+      // JavaScript Questions
+      {
+        question: "What is the 'DOM' in JavaScript?",
+        options: ["Document Object Model", "Data Object Model", "Dynamic Operation Mechanism", "Digital Object Memory"],
+        answer: 1,
+      },
+      {
+        question: "What is an event handler in JavaScript?",
+        options: ["A function that handles errors", "A function that responds to user actions", "A variable that stores events", "A loop that iterates through events"],
+        answer: 2,
+      },
+      {
+        question: "What is the purpose of the 'let' keyword in JavaScript?",
+        options: ["Declaring a constant variable", "Creating a block-scoped variable", "Defining a function", "Importing external libraries"],
+        answer: 2,
+      },
+    
+      // Operating Systems Questions
+      {
+        question: "What is a process in the context of operating systems?",
+        options: ["A running application", "A unit of execution", "A folder on the desktop", "A software license"],
+        answer: 2,
+      },
+      {
+        question: "What is the role of the kernel in an operating system?",
+        options: ["Managing hardware resources", "Providing a user interface", "Executing application code", "Creating backup copies"],
+        answer: 1,
+      },
+      {
+        question: "What is a page fault in virtual memory systems?",
+        options: ["A hardware failure", "A mistake in programming", "A request for data that is not in physical memory", "A security vulnerability"],
+        answer: 3,
+      },
+    
+      // Additional Questions
+      {
+        question: "What is the purpose of SQL in databases?",
+        options: ["Styling web pages", "Querying and managing data", "Creating animations", "Building user interfaces"],
+        answer: 2,
+      },
+      {
+        question: "In networking, what is the purpose of the OSI model?",
+        options: ["Managing software updates", "Providing a framework for understanding network functions", "Controlling access to the internet", "Encrypting data transmissions"],
+        answer: 2,
+      },
+
+
+
+
+];
